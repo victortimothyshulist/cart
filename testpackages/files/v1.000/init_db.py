4f7778adba02ed605179a77b2c7a5610eb38aff4
victortimothyshulist <vtshulist@gmail.com>
2022-12-24 11:44:10 -0500

    added dos2unix

--- testpackages/files/v1.000/init_db.py
+++ testpackages/files/v1.000/init_db.py
@@ -1,3 +1,8 @@
+os.system("dos2unix ./clearKB.py")
 os.system("chmod +x ./clearKB.py")
 os.system("./clearKB.py -YES")
+
+os.system("dos2unix setupdb.py")
+os.system("chmod +x setupdb.py")
+os.system("./setupdb.py")
 
