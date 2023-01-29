ed92ce9bb623f4141b957920d9c9dc5ba1646f8b
victortimothyshulist <vtshulist@gmail.com>
2023-01-29 10:18:04 -0500

    Update - we -ARE- allowing a term to be in more than one synonym set

--- testpackages/files/v1.000/dump_lexicon_dict.py
+++ testpackages/files/v1.000/dump_lexicon_dict.py
@@ -5,8 +5,12 @@ for _CART_lex_txt in sorted(lexicon_dict.keys()):
    for _CART_lex_grp in sorted(lexicon_dict[_CART_lex_txt]['groups']):
       _CART_lex_grps += _CART_lex_grp + ','
 
+   _CART_lex_synsets = ""
+   for _CART_synset in sorted(lexicon_dict[_CART_lex_txt]['synsetids']):
+      _CART_lex_synsets += _CART_synset
+
    _CART_lex_grps = _CART_lex_grps[0:-1]
 
-   _CART_lex += _CART_lex_txt + ':GROUPS=' + _CART_lex_grps + ':constant_strings=' + str(lexicon_dict[_CART_lex_txt]['constant_strings.dat']) + ':symbolset=' + str(lexicon_dict[_CART_lex_txt]['synsetid']) + "\n"
+   _CART_lex += _CART_lex_txt + ':GROUPS=' + _CART_lex_grps + ':constant_strings=' + str(lexicon_dict[_CART_lex_txt]['constant_strings.dat']) + ':symbolsets=' + _CART_lex_synsets + "\n"
 
 cartlog("lexicon", _CART_lex)
--- testpackages/files/v1.000/reject_mult_synset.py
+++
@@ -1,8 +0,0 @@
-_CART_lst_synsets = list()
-_CART_lst_synsets.append(saw_member_in_syn_set[term])
-_CART_lst_synsets.append(atid)
-_CART_lst_synsets_sorted = sorted(_CART_lst_synsets)
-
-cartlog("multsynset", "In synonym set database - term '" + term + "' came up twice! All terms can only belong to zero or at most one synonym set. The syn sets it belongs to are: " + str(_CART_lst_synsets_sorted))
-
-
